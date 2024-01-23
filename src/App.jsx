e56e752a8b4e73343030157201c5ebe10c6a1809
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-01-24 00:31:10 +1300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div className='container'>
+			<h1 className='text-3xl text-bold text-center capitalize'>
+				App goes here
+			</h1>
+		</div>
+	);
+};
+
+export default App;
