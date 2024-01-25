e4b5d27a605fcbbd09f4900c03dc7c69157b059f
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-01-26 11:35:33 +1300

    created a data file with the list of links

---
+++ src/data.jsx
@@ -0,0 +1,9 @@
+const links = [
+	{ id: 1, text: 'Github', href: 'https://www.github.com/' },
+	{ id: 2, text: 'Frontend Mentor', href: 'https://www.frontendmentor.io/' },
+	{ id: 3, text: 'LinkedIn', href: 'https://www.linkedin.com/' },
+	{ id: 4, text: 'Twitter', href: 'https://www.twitter.com/' },
+	{ id: 5, text: 'Instagram', href: 'https://www.instagram.com/' },
+];
+
+export default links;
