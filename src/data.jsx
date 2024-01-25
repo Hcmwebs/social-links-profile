c810c65c5e9c80123c86ade4d2069cd26600ca21
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-01-26 11:38:11 +1300

    modified the href of the some links

--- src/data.jsx
+++ src/data.jsx
@@ -1,9 +1,9 @@
 const links = [
 	{ id: 1, text: 'Github', href: 'https://www.github.com/' },
 	{ id: 2, text: 'Frontend Mentor', href: 'https://www.frontendmentor.io/' },
-	{ id: 3, text: 'LinkedIn', href: 'https://www.linkedin.com/' },
+	{ id: 3, text: 'LinkedIn', href: '' },
 	{ id: 4, text: 'Twitter', href: 'https://www.twitter.com/' },
-	{ id: 5, text: 'Instagram', href: 'https://www.instagram.com/' },
+	{ id: 5, text: 'Instagram', href: '' },
 ];
 
 export default links;
